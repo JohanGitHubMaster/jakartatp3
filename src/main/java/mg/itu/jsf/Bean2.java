3dc913dd33e0304b7708c43df0e62df4025cf40f
Johan <>
2023-04-04 19:33:08 +0300

    ajout de l'exercice 4

--- src/main/java/mg/itu/jsf/Bean2.java
+++ src/main/java/mg/itu/jsf/Bean2.java
@@ -40,7 +40,7 @@ public class Bean2 {
     }
 
     public String afficher() {
-        return "affichage_4?nb=" + nombre + "&amp;faces-redirect=true";
+        return "affichage__4?nb=" + nombre + "&amp;faces-redirect=true";
     }
 
     public Bean2() {
